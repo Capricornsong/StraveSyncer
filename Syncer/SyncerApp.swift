3117798d65b027f8bd97e1ea3709df0ed5fd5fe5
capricornsong <lius.he@foxmail.com>
2026-05-30 20:07:19 +0800

    Initial Commit

---
+++ Syncer/SyncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncerApp.swift
+//  Syncer
+//
+//  Created by Stefan He on 2026/5/30.
+//
+
+import SwiftUI
+
+@main
+struct SyncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
